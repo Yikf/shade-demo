b369062e4928abc1c223751f82c7828aad5b9335
yikaifei <yikaifei@baidu.com>
2021-10-25 15:21:51 +0800

    test String.startWith

--- release-core/src/main/java/com/baidu/shade/ShadeDemo.java
+++ release-core/src/main/java/com/baidu/shade/ShadeDemo.java
@@ -4,7 +4,11 @@ import com.alibaba.fastjson.JSONObject;
 
 public class ShadeDemo {
     public static void main(String[] args) {
+        String a = "shade.org.apache";
 
+        System.out.println(a.startsWith("shade"));
+
+        System.out.println(a.substring(6));
     }
 
     public void castJson() {
